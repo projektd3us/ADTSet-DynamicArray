f90f650763f23ee465c3d77fc1b3cb9db411660c
Dennis Davis <71829402+projektd3us@users.noreply.github.com>
2021-03-14 18:15:01 +0200

    Add project files.

---
+++ res/App.cpp
@@ -0,0 +1,32 @@
+#include "Set.h"
+#include "SetIterator.h"
+#include "ExtendedTest.h"
+#include "ShortTest.h"
+#include <stack>
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+	testAll();
+	testAllExtended();
+
+	//Set s;
+	//s.add(5);
+	//s.add(3);
+	//s.add(3);
+	//s.add(3);
+	//s.add(4);
+	//s.remove(3);
+	//s.print();
+
+	cout << "That's all!" << endl;
+	system("pause");
+
+}
+
+
+
---
+++ res/Set.cpp
@@ -0,0 +1,100 @@
+#include "Set.h"
+#include "SetITerator.h"
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+void Set::remPlace(int pos) { // function to remove item at pos in array
+	for (int i = pos; i <= this->length - 2; i++) {
+		this->arr[i] = this->arr[i + 1]; // shift right
+	}
+}
+
+
+Set::Set() {
+	this->capacity = 100;
+	this->length = 0;
+	this->arr = (TElem*)malloc(100 * sizeof(TElem));
+}
+
+
+bool Set::add(TElem elem) {
+	//check if exists
+	if (this->search(elem)) {
+		return false; // skip ahead
+	}
+	else {
+		//if cap full realloc
+		if (this->capacity == this->length) {
+			int newCapacity = this->capacity * 2;
+			this->arr = (TElem*)realloc(this->arr, newCapacity * sizeof(TElem));
+			this->capacity = newCapacity;
+		}
+		this->arr[this->length] = elem; // add to set
+		this->length++; // change element number
+		return true;
+	}
+}
+
+
+bool Set::remove(TElem elem) { // fakes removal by shortening length
+	int pos = -1; //untouchable
+	for (int i = 0; i <= this->length - 1; i++) {
+		if (this->arr[i] == elem) {
+			pos = i; // skip ahead
+			break;
+		}
+	}
+	if (pos == -1) { // not found
+		return false;
+	}
+	else { // found
+		remPlace(pos); //remove
+		this->length--; //count as removed
+		return true; //exit with true
+	}
+
+}
+
+
+bool Set::search(TElem elem) const {
+	for (int i = 0; i <= this->length - 1; i++) {
+		if (this->arr[i] == elem) {
+			return true; // skip ahead
+		}
+	}
+	return false; // nothing found
+}
+
+
+int Set::size() const {
+	return this->length;
+}
+
+
+bool Set::isEmpty() const {
+	if (this->length == 0) {
+		return true;
+	}
+	return false;
+}
+
+
+Set::~Set() {
+	delete[] this->arr;
+}
+
+void Set::print() const
+{
+	for (int i = 0; i <= this->length - 1; i++) {
+		cout << this->arr[i] << " ";
+	}
+}
+
+
+SetIterator Set::iterator() const {
+	return SetIterator(*this);
+}
+
+
